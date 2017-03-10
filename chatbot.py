ce6d2c5789687b8e62c2feffa46f885f4ba712d9
rama <com.ramona@yahoo.com>
2017-03-10 16:36:08 +0000

    cleanup

--- chatbot.py
+++ chatbot.py
@@ -6,17 +6,16 @@ from nltk.corpus import gutenberg
 import cPickle as pickle
 
 from gensim.models import Word2Vec
-from nltkx import NgramModel
+
 
 nltk.data.path.append('./my_nltk_data/')
 
 class Edi(object):
     def __init__(self, file="austen-persuasion.txt"):
 
-        #tokens = gutenberg.words(file)
-        #print "tokenized"
         writepath="save.p"
         mode = 'r' if os.path.exists(writepath) else 'wb'
+        # check if model already saved
         with open(writepath, mode) as f:
             if mode =='r':
                 self.model = pickle.load(f)
@@ -29,10 +28,9 @@ class Edi(object):
             
 
     def respond_to(self, message):
-        # Pick a random word from the incoming message
+        # Find similar wordss
         input_tokens = nltk.word_tokenize(message)
         ans = []
-        #input_tokens = filter(lambda x: len(x)>2, input_tokens)
         for inp in input_tokens:
             if len(inp)>2:
                 try:
@@ -41,24 +39,7 @@ class Edi(object):
                     ans.append(inp)
             else:
                 ans.append(inp)
-        # keyword = random.choice(input_tokens)
 
-        # # Use keyword and input length to seed a response
-        # input_length = len(input_tokens)
-        # num_response_words = int(random.gauss(input_length, input_length / 2)) + 1
-        # print "Basing response on keyword %s and length %d" % (keyword, num_response_words)
-        # content = self._model.generate(num_response_words, (keyword,))
-        # return self._format_response(content)
         return " ".join(ans)
 
-    def _format_response(self, content):
-        
-        def to_unicode(x):
-            if isinstance(x, str):
-                return x.decode('utf-8')
-            return x
-
-        print content
-        s = u' '.join([to_unicode(c) for c in content])
-        s = re.sub(r' ([\?,\.:!])', r'\1', content)  # Remove spaces before separators
-        return content
+   

a9705cc14136127704e25f9f1dace191b581df31
rama <com.ramona@yahoo.com>
2017-03-10 18:11:33 +0000

    trying

--- chatbot.py
+++ chatbot.py
@@ -87,6 +87,8 @@ class Edi(object):
         print message_text
         if "hi" in message_text.lower() or "hello" in message_text.lower():
             answer = "Hello to you too. Type a message or a message beginning with 'syn' or a message beginning with 'def'."
+            return answer
+            
         if "syn" in message_text[:3]:
             answer = self.get_synonyms_or_def(message_text[3:])
         elif "def" in message_text[:3]:
